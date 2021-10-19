153070fe566129b60910d0105eff7a8733b8045f
Ryo Fuj <ryo176999@gmail.com>
2021-10-19 12:33:47 -0700

    TaskManagingApp init
    create xcode project

---
+++ TaskManagingApp/TaskManagingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagingAppApp.swift
+//  TaskManagingApp
+//
+//  Created by ryo fujimura on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
